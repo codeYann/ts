271e80b3a3eff69efa2e1553a45e5cded9abdda2
codeYann <ccyan05@alu.ufc.br>
2022-02-22 22:46:20 -0300

    Data types in ts

---
+++ src/types/main.ts
@@ -0,0 +1,10 @@
+// Tipos primitivos: string, number (float, int), boolean
+
+const all_types = {
+  a: 42,
+  b: "that's life",
+  c: true,
+  d: [42, 43, 44],
+};
+
+console.log(all_types.d, typeof (all_types.d));
