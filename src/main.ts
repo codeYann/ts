658505d8943a3aaf6234497507171412f1e2a9ec
codeYann <ccyan05@alu.ufc.br>
2022-03-26 14:44:43 -0300

    Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import {printId, getId} from './types/index';
+import {
+  printId,
+  getId,
+  createInternalObject,
+  internalObject,
+  showInternalObject,
+} from './types/index';
 
 printId(10);
 printId([10, 15, 25, 30]);
+
 console.log(getId(10));
 console.log(getId([10, 15, 25, 30]));
+
+const obj: internalObject = createInternalObject(10, true, 'H2456JAKS$$S');
+showInternalObject(obj);
