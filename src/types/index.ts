70de6896e590a0842d250292aa83f68645500195
codeYann <ccyan05@alu.ufc.br>
2022-03-26 14:44:36 -0300

    Learning type alias, passing through function args and creating new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,10 @@
  *
 */
 
-export function printId(id: number | number[]): void {
+type credential = number | number[]; // we can use type alias to define new type
+export type internalObject = {id: number, hash: string, status: boolean};
+
+export function printId(id: credential): void {
   if (typeof(id) == 'number') {
     console.log(`Your id is ${id}`);
     return;
@@ -11,4 +14,23 @@ export function printId(id: number | number[]): void {
   id.forEach((item) => console.log(`All id's: ${item}`));
 }
 
-export const getId = (id: number | number[]): (number | number[]) => id;
+export const getId = (id: credential): credential => id;
+
+export const createInternalObject = (
+    id: number,
+    status: boolean,
+    hash: string): internalObject => {
+  return {
+    id,
+    status,
+    hash,
+  };
+};
+
+export const showInternalObject = (object: internalObject): void => {
+  for (const k in object) {
+    if (Object.prototype.hasOwnProperty.call(object, k)) {
+      console.log(k);
+    }
+  }
+};
