8250eae30577dc7072da2ac785a28f28fa0ad165
codeYann <ccyan05@alu.ufc.br>
2022-03-31 09:42:56 -0300

    Learning how interfaces in typescript works

--- src/types/interface-test.ts
+++ src/types/interface-test.ts
@@ -1,23 +1,28 @@
-/** 
+/**
  * We can use interface to define an object type
- * 
  */
 
 export interface Person {
+  height: number;
+  age: number;
+  name: string;
+}
+
+export function createPerson(
   height: number,
   age: number,
   name: string,
-}
-
-export function createPerson(height: number, age: number, name: string): Person {
+): Person {
   return {
     height,
     age,
-    name
+    name,
   };
 }
 
 export function greetingPerson(person: Person) {
-  console.log(`What up dude. You're ${person.name}, ${person.age}, ${person.height}`);
+  console.log(
+    `What up dude. You're ${person.name}, ${person.age}, ${person.height}`,
+  );
 }
 
