e57851cf21309e39f9aa5bab19d3ae9f67ce3258
codeYann <ccyan05@alu.ufc.br>
2022-03-31 09:43:08 -0300

    Types in typescript

--- src/types/types-test.ts
+++ src/types/types-test.ts
@@ -1,13 +1,12 @@
 /**
  * We can combine types in functions arguments and also in return types.
- *
-*/
+ */
 
 type credential = number | number[]; // we can use type alias to define new type
-export type internalObject = {id: number, hash: string, status: boolean};
+export type internalObject = { id: number; hash: string; status: boolean };
 
 export function printId(id: credential): void {
-  if (typeof(id) == 'number') {
+  if (typeof (id) == 'number') {
     console.log(`Your id is ${id}`);
     return;
   }
@@ -17,9 +16,10 @@ export function printId(id: credential): void {
 export const getId = (id: credential): credential => id;
 
 export const createInternalObject = (
-    id: number,
-    status: boolean,
-    hash: string): internalObject => {
+  id: number,
+  status: boolean,
+  hash: string,
+): internalObject => {
   return {
     id,
     status,
@@ -34,3 +34,4 @@ export const showInternalObject = (object: internalObject): void => {
     }
   }
 };
+
